cf5290fada2f783cd732a22932c356232ce98201
GuilhermeRibSouza <117588773+GuilhermeRibSouza@users.noreply.github.com>
2022-12-10 19:29:26 -0300

    Create PAUSA.py

---
+++ Exercicios - Python/PAUSA.py
@@ -0,0 +1,4 @@
+'''JUSTIFICANDO: 
+Consegui um novo emprego, devido a pesada carga horaria, mudarei meus exercicios para o fim de semana, mas não desistirei de estudar
+ainda assim, praticarei um pouco de python no IDE do celular, para não enferrujar.
+Quando o trabalho estiver mais leve e eu me habituar a ele, volto a normalidade.'''
